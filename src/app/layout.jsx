bda0e34c76985d533268c9fd3b3ebfd8465ea6cd
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-09 14:46:56 -0300

    feat: modify layout styles and page content

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#f5d04e] flex flex-col justify-center items-center min-h-screen py-20`}>{children}</body>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,37 @@
+import GenericMediaContainer from "@/components/GenericMediaContainer";
+import IllustrationPreview from "@/components/IllustrationPreview";
+import imgAvatar from "@/assets/image-avatar.webp";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Blog Preview Card</h1>
+    <main className="flex flex-col items-left border border-black bg-white rounded-lg p-6 shadow max-w-[300px] gap-2">
+      <GenericMediaContainer
+        width="260px"
+        className="mb-4 rounded-lg overflow-hidden"
+      >
+        <IllustrationPreview />
+      </GenericMediaContainer>
+      <span className="bg-[#f5d34a] font-extrabold p-2 text-black w-fit h-fit rounded-md">Learning</span>
+      <small>Published 21 Dec 2023</small>
+      <h2 className="text-1xl font-extrabold">HTML & CSS foundations</h2>
+      <p>
+        These languages are the backbone of every website, defining structure,
+        content, and presentation.
+      </p>
+      <div className="flex items-center gap-2">
+        <GenericMediaContainer width="30px" height={"30px"}>
+          <Image
+            src={imgAvatar}
+            alt="Greg Hooper"
+            width="900px"
+            height="900px"
+            className="rounded-full"
+          />
+        </GenericMediaContainer>
+
+        <h3 className="font-bold">Greg Hooper</h3>
+      </div>
     </main>
   );
 }
